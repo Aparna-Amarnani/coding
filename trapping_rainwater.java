bdc4395920c5bcdc43420176979db3e7ad90cf01
Aparna Amarnani <71811435+Aparna-Amarnani@users.noreply.github.com>
2022-12-12 17:06:08 +0530

    Update trapping_rainwater.java

--- trapping_rainwater.java
+++ trapping_rainwater.java
@@ -3,6 +3,7 @@ class Solution {
         int[] left=new int[height.length];
         int[] right=new int[height.length];
         int leftmax=0,rightmax=0;
+        //auxilary array to find max height to the left of any block
         for(int i=0;i<height.length;i++)
         {
             if(height[i]>leftmax)  
@@ -11,6 +12,7 @@ class Solution {
             }
             left[i]=leftmax;
         }
+        ////auxilary array to find max height to the right of any block
         for(int i=height.length-1;i>=0;i--)
         {
             if(height[i]>rightmax)
@@ -19,6 +21,8 @@ class Solution {
             }
             right[i]=rightmax;
         }
+        //water can be stored only if there is a block higher than the current block on its left or right
+        //amt of water that can be stored is the min height of the left and right block minus the height of the current block
         int amt=0;
         for(int i=0;i<height.length;i++)
         {
